4c83095d517a367e19e29320a14c46ad217108d2
gold-expirience-requiem <148878081+gold-expirience-requiem@users.noreply.github.com>
2023-11-02 11:40:56 -0700

    Update lesson_3.py

--- lesson_3.py
+++ lesson_3.py
@@ -1,28 +1,36 @@
 import discord
+from discord.ext import commands
 from lesson2 import gen_pass
 
-# Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
-# Включаем привелегию на чтение сообщений
 intents.message_content = True
-# Создаем бота в переменной client и передаем все привелегии
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$password'):
-        await message.channel.send(gen_pass(10))
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
 
-client.run("MTE2ODkwNjE0MzE5Njc4MjY3NA.GQrg4c.3oSMwHqPN3Nm6r_c4mDZOeRVneLagTKql5vJ0U")
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx, len_pass = 7):
+    await ctx.send(gen_pass(len_pass))
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def add_str(ctx, left, right):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("MTE2ODkwNjE0MzE5Njc4MjY3NA.GQrg4c.3oSMwHqPN3Nm6r_c4mDZOeRVneLagTKql5vJ0U")
